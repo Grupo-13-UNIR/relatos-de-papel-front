8f32a4fef102a17f35fb0590f1413474b8cc84ff
César Rodríguez Vilagrá <128821412+CesarRodrigu@users.noreply.github.com>
2026-04-24 18:10:58 +0200

    Add shadcn, tailwind

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,11 @@
 import js from '@eslint/js';
-import globals from 'globals';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
 import { defineConfig, globalIgnores } from 'eslint/config';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import noRelativeImportPaths from 'eslint-plugin-no-relative-import-paths';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -17,11 +18,23 @@ export default defineConfig([
       reactRefresh.configs.vite,
       eslintPluginPrettierRecommended,
     ],
+    plugins: {
+      'no-relative-import-paths': noRelativeImportPaths,
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     rules: {
+      'no-relative-import-paths/no-relative-import-paths': [
+        'error',
+        {
+          allowSameFolder: false,
+          rootDir: 'src',
+          prefix: '@/',
+        },
+      ],
+
       'prettier/prettier': [
         'error',
         {
